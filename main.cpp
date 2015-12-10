4e541dc58e8ac202f0f3a1d9fbfc0e2fbae3344f
YMM256 <a-mutou@gd6.so-net.ne.jp>
2015-12-10 23:00:06 +0900

    WindowsでのQt Creatorの動作が不安なので、とりあえずコミット メモリ転送のベンチマークを実装予定。（P2PとSHMの比較を行う。）

--- main.cpp
+++ main.cpp
@@ -4,29 +4,58 @@
 #include <numeric>
 #include "MPIWapperUtils.h"
 #include "MPISharedVector.h"
+#include <QElapsedTimer>
+
+const int ExecCount = 100;
 
 int main(int argc, char *argv[])
 {
     //! MPIの初期化
     MPI_Init( &argc, &argv );
 
-    MPISharedVector<double>     vec( 100, 0.0 );
+    std::vector<double> bufferIn( 1 << 20 );
+    std::vector<double> bufferOut( 1 << 20 );
 
-    if( MPIRank() == 0 )
+    std::cout << "rank " << MPIRank() << " " << bufferIn.size() << std::endl;
+
+    int src = MPIRank() - 1;
+    int dst = MPIRank() + 1;
+
+    if( src < 0 )
     {
-        vec[9] = 12.8;
-        vec[10] = 0.9998;
+        src = MPISize() - 1;
+    }
+
+    if( dst >= MPISize() )
+    {
+        dst = 0;
+    }
+
+    MPI_Request request[2];
+    qint64 elapsed = 0;
+
+    for( auto ii = 0; ii < ExecCount; ii++ )
+    {
+        QElapsedTimer timer;
+        timer.start();
+
+        MPI_Isend( bufferOut.data(), bufferIn.size(),  MPI_DOUBLE, dst, 0, MPI_COMM_WORLD, &request[0] );
+        MPI_Irecv( bufferIn.data() , bufferOut.size(), MPI_DOUBLE, src, 0, MPI_COMM_WORLD, &request[1] );
+
+        MPI_Waitall( sizeof( request ), request, MPI_STATUSES_IGNORE );
+
+        elapsed += timer.elapsed();
+
+        MPI_Barrier( MPI_COMM_WORLD );
     }
 
     MPI_Barrier( MPI_COMM_WORLD );
 
-    if( MPIRank() == 1  )
-    {
-        double val = std::accumulate( vec.begin(), vec.end(), 0.0 );
-        vec[0] = val;
-    }
+    std::cout << "Rank " << MPIRank() << ": " << elapsed << std::endl;
 
-    std::cout << vec[0] << std::endl;
+
+//    MPISharedVector<double> vecIn( (1 << 20) * MPISize() );
+//    MPISharedVector<double> vecOut( (1 << 20) * MPISize() );
 
     //! ラップしたMPI_Finalize()の呼び出し。
     MPIFinalize();
